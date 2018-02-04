fe46a5271d2ad36b2790e90e63400893e85b8bec
Tristan <tristan.henry@outlook.com>
2018-02-04 22:25:52 +1100

    GetAndCleanData
    Peer-grade

---
+++ 3GetAndCleanData/W4/README.md
@@ -0,0 +1,44 @@
+# Peer-graded Assignment: Getting and Cleaning Data Course Project
+
+The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set on the course *Getting and Cleaning Data*.
+
+## Review criteria
+
+1. The submitted data set is tidy.
+2. The Github repo contains the required scripts.
+3.  GitHub contains a code book that modifies and updates the available codebooks with the data to indicate all the variables and summaries calculated, along with units, and any other relevant information.
+4. The README that explains the analysis files is clear and understandable.
+5. The work submitted for this project is the work of the student who submitted it.
+
+```
+Tidy data is formatted in a standard way that facilitates exploration and analysis and works seamlessly with other tidy data tools. Specifically, tidy data
+satisfies three conditions: 
+1) Each variable forms a column     
+2) Each observation forms a row
+3) Each type of observational unit forms a table.
+```
+
+## Prerequisites
+
+R installed with the following libraries:
+* data.table
+* dplyr
+
+## How to test it
+
+Run the following R file: 
+```
+run_analysis.R
+```
+
+## Links
+
+* Repo: [GetAndCleanData](https://github.com/tristanry/datasciencecoursera/3GetAndCleanData/W4)
+* Script: [run_analysis.R](run_analysis.R)
+* Readme: [README.md](README.md)
+* Code book [CodeBook.md]
+* Tidy dataset [tidydata.csv](tidydata.csv)
+
+## Authors
+
+[*Tristan HENRY*](https://github.com/tristanry)
---
+++ 3GetAndCleanData/W4/run_analysis.R
@@ -0,0 +1,53 @@
+# Merges the training and the test sets to create one data set.
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive variable names.
+# From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+library(data.table)
+
+#collect & unzip dataset  
+download.file(url="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip ", destfile = "./dataset.zip")
+unzip("./dataset.zip")
+
+#get labels & features 
+zipdir<-"UCI HAR Dataset"
+
+dt_activity_label <- data.table::fread(paste(zipdir, "/activity_labels.txt", sep =""), col.names = c("class", "activity"))
+dt_feature <- data.table::fread(paste(zipdir, "/features.txt", sep =""), col.names = c("id", "feature"))
+
+#select mean & std columns
+index_meanstd <- grep("(mean|std)\\(\\)", dt_feature[, feature])
+feature_meanstd <- gsub('[()-]', '', dt_feature[index_meanstd, feature])
+
+## (2). Extracts only the measurements on the mean and standard deviation for each measurement.
+## (5). Appropriately labels the data set with descriptive variable names. 
+#get train data 
+type <- "train"
+dt_train_set <- data.table::fread(paste(zipdir, "/", type,"/X_", type, ".txt", sep =""), select = index_meanstd, col.names = feature_meanstd)
+dt_train_activity <- data.table::fread(paste(zipdir, "/", type,"/y_", type, ".txt", sep =""), col.names = c("class"))
+dt_train_subject <- data.table::fread(paste(zipdir, "/", type,"/subject_", type, ".txt", sep =""), col.names = c("subject"))
+
+dt_train_set <- cbind(dt_train_subject, dt_train_activity, dt_train_set)
+
+#get test data 
+type <- "test"
+dt_test_set <- data.table::fread(paste(zipdir, "/", type,"/X_", type, ".txt", sep =""), select = index_meanstd, col.names = feature_meanstd)
+dt_test_activity <- data.table::fread(paste(zipdir, "/", type,"/y_", type, ".txt", sep =""), col.names = c("class"))
+dt_test_subject <- data.table::fread(paste(zipdir, "/", type,"/subject_", type, ".txt", sep =""), col.names = c("subject"))
+
+dt_test_set <- cbind(dt_test_subject, dt_test_activity, dt_test_set)
+
+## (1). Merges the training and the test sets to create one data set.
+dt_set    <- rbind(dt_train_set, dt_test_set)
+
+## (3). Uses descriptive activity names to name the activities in the data set
+dt_set <- merge(x = dt_set, y = dt_activity_label, by = "class", all.x = TRUE)
+
+## (5). From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# library(reshape2)
+library(dplyr)
+dt_set_mean <- dt_set %>% group_by(class, subject, activity) %>% summarise_all(funs(mean))
+
+# write the tidy dataset 
+data.table::fwrite(x = dt_set_mean, file = "tidydata.csv", quote = FALSE, sep = ",")
